701ad8632626b27caa1f2907825ccd885dc724ea
jobearry <golimlim2209@gmail.com>
2025-06-22 00:47:05 +0800

    Update requirements and fix import path for UserResponse model

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from typing import List
-from models.user import UserResponse
+from src.models.user import UserResponse
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from dotenv import load_dotenv
